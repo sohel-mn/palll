4a3fc65352ca4d3b4cb8d32eb3d81dc1cb6a2926
sohel-mn <84955810+sohel-mn@users.noreply.github.com>
2022-06-24 10:16:53 +0600

    Add files via upload
    erferhfs

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Mixit up
+
+var mixer = mixitup('.work_grid');
+
+// hamburger menu
+
+function openMenu(){
+    document.getElementById('hamburger_menu').style.width = '100%';
+}
+function closeMenu(){
+    document.getElementById('hamburger_menu').style.width = '0%';
+}
